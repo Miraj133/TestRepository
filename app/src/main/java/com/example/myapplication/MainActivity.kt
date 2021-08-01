33559b09841466470535ba45b35d3b73dbb2e282
Miraj133 <Miraj133@yandex.ru>
2021-08-01 20:53:00 +0500

    Готовый вариант

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,20 +4,19 @@ import android.os.Bundle
 import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(),MainNavigation {
+class MainActivity : AppCompatActivity(), MainNavigation {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         openAuthFragment()
     }
 
-     override fun openAuthFragment() {
+    override fun openAuthFragment() {
         supportFragmentManager
             .beginTransaction()
             .add(R.id.clRoot, AuthFragment())
             .commit()
     }
-
 
 
     override fun openOrderFragment(etPhone: String) {
