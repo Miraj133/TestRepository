88a9c563965fdee054049e041964c9a08cbb2ce4
Miraj133 <Miraj133@yandex.ru>
2021-08-07 05:54:23 +0500

    Доделан экран диалога

--- app/src/main/java/com/example/myapplication/DishDialog.kt
+++ app/src/main/java/com/example/myapplication/DishDialog.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class DishDialog: BottomSheetDialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        return super.onCreateDialog(savedInstanceState)
+        return BottomSheetDialog(requireContext(), theme).apply {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
