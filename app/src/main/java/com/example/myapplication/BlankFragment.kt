20f26ef597fe9b2d97a054f52b618a19130fba98
Miraj133 <Miraj133@yandex.ru>
2021-10-05 23:59:26 +0500

    начало

---
+++ app/src/main/java/com/example/myapplication/BlankFragment.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.FragmentBlankBinding
+
+class BlankFragment: Fragment( ) {
+    lateinit var binding: FragmentBlankBinding
+}
